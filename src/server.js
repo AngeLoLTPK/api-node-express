da8b3f0304daf2f35c26f80bc777bbc508f7af94
Angelo Machado <angelogm2005@gmail.com>
2023-08-07 17:54:26 -0300

    nodemon abrindo servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log("Olá mundo, servidor rodando");
+const express = require('express');
+
+const app = express();
+app.listen(3000, () => {
+    console.log("Server Working in door 3000. http://localHost:3000")
+})
+
+app.get('/', (req, res) => {
+    res.json({message: 'welcome to our world API'});
+});
